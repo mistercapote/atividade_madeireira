726f2819abf8d0687496291abf333c7a005994a0
mistercapote <luizleb70@gmail.com>
2024-11-12 23:58:36 -0300

    Separando nodes e edges

--- src/clean.py
+++ src/clean.py
@@ -17,10 +17,13 @@ destino = df[["CPF_CNPJ_Des", "LatDestino", "LongDestino", "TpDes"]].rename(
     'TpDes': 'Tipo'
 })
 nodes = pd.concat([origem, destino], ignore_index=True)
-nodes.drop_duplicates("CPF_CNPJ")
+nodes.drop_duplicates("CPF_CNPJ", inplace=True)
+nodes["Coordenada"] = nodes.apply(lambda row: (row['Latitude'], row['Longitude']), axis=1)
+nodes.drop(["Latitude", "Longitude"], axis=1, inplace=True)
 nodes.to_csv("nodes.csv", index=False)
 
 edges = df[["CPF_CNPJ_Rem", "CPF_CNPJ_Des", "Produto", "Volume", "DtEmissao"]]
+edges = edges[edges['CPF_CNPJ_Rem'] != edges['CPF_CNPJ_Des']]
 # edges["DtEmissao"] = pd.to_datetime(edges["DtEmissao"], yearfirst=True)
 edges.sort_values(['DtEmissao'], ascending=True, inplace=True)
 edges.to_csv("edges.csv", index=False)
