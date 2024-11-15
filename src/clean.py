a99983cd2ac925755830428d1a7d8e53c626f80a
mistercapote <luizleb70@gmail.com>
2024-11-15 16:52:00 -0300

    Clean atualizado

--- src/clean.py
+++ src/clean.py
@@ -1,29 +1,37 @@
 import pandas as pd
 
-df = pd.read_csv("data/df_01.csv", low_memory=False)
+df1 = pd.read_csv("data/df_01.csv", low_memory=False)
+df2 = pd.read_csv("data/df_02.csv", low_memory=False)
+df = pd.concat([df1, df2], ignore_index=True)
 
 origem = df[["CPF_CNPJ_Rem", "LatOrigem", "LongOrigem", "TpRem"]].rename(
     columns={
-    'CPF_CNPJ_Rem': 'CPF_CNPJ', 
-    'LatOrigem': 'Latitude', 
-    'LongOrigem': 'Longitude', 
-    'TpRem': 'Tipo'
+    'CPF_CNPJ_Rem': 'id_emp', 
+    'LatOrigem': 'latitude', 
+    'LongOrigem': 'longitude', 
+    'TpRem': 'type'
 })
 destino = df[["CPF_CNPJ_Des", "LatDestino", "LongDestino", "TpDes"]].rename(
     columns={
-    'CPF_CNPJ_Des': 'CPF_CNPJ', 
-    'LatDestino': 'Latitude', 
-    'LongDestino': 'Longitude', 
-    'TpDes': 'Tipo'
+    'CPF_CNPJ_Des': 'id_emp', 
+    'LatDestino': 'latitude', 
+    'LongDestino': 'longitude', 
+    'TpDes': 'type'
 })
 nodes = pd.concat([origem, destino], ignore_index=True)
-nodes.drop_duplicates("CPF_CNPJ", inplace=True)
-nodes["Coordenada"] = nodes.apply(lambda row: (row['Latitude'], row['Longitude']), axis=1)
-nodes.drop(["Latitude", "Longitude"], axis=1, inplace=True)
+nodes.drop_duplicates("id_emp", inplace=True)
 nodes.to_csv("nodes.csv", index=False)
 
-edges = df[["CPF_CNPJ_Rem", "CPF_CNPJ_Des", "Produto", "Volume", "DtEmissao"]]
-edges = edges[edges['CPF_CNPJ_Rem'] != edges['CPF_CNPJ_Des']]
-# edges["DtEmissao"] = pd.to_datetime(edges["DtEmissao"], yearfirst=True)
-edges.sort_values(['DtEmissao'], ascending=True, inplace=True)
+edges = df[["CPF_CNPJ_Rem", "CPF_CNPJ_Des", "Produto", "Volume", "DtEmissao"]].rename(
+    columns={
+        "CPF_CNPJ_Rem": "node_src",
+        "CPF_CNPJ_Des": "node_dest",
+        "Produto": "id_product",
+        "Volume": "vol",
+        "DtEmissao": "date"
+    }
+)
+edges = edges[edges['node_src'] != edges['node_dest']]
+edges["date"] = pd.to_datetime(edges["date"], yearfirst=True)
+edges.sort_values(['date'], ascending=True, inplace=True)
 edges.to_csv("edges.csv", index=False)
