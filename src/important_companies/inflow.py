aa3c509f6976a3f5590be35db0f0fb81bae7ec19
JaimeWillianCarneiro <jwcs2@aluno.ifal.edu.br>
2024-11-20 01:01:35 -0300

    finish for now

--- src/important_companies/inflow.py
+++ src/important_companies/inflow.py
@@ -9,9 +9,14 @@ from tqdm import tqdm
 from modules import  get_concessions, get_sink_nodes, get_timberflow
 from data_cleaner import convert_id_to_str
 
+# Lendo os csvs
+transportes_janeiro =  pd.read_csv('data/df_01.csv')
+transportes_feveiro = pd.read_csv('data/df_02.csv')
+transportes_marco = pd.read_csv('data/df_03.csv')
+transportes_abril = pd.read_csv('data/df_04.csv')
+transportes_maio = pd.read_csv('data/df_05.csv')
+transportes_junho = pd.read_csv('data/df_06.csv')
 
-transportes_janeiro =pd.read_csv('data/df_01.csv') 
-# transportes_junho = pd.read_csv('../data/df_06.csv')
 transportes_julho = pd.read_csv('data/df_07.csv')
 transportes_agosto = pd.read_csv('data/df_08.csv')
 transportes_setembro = pd.read_csv('data/df_09.csv')
@@ -20,10 +25,10 @@ transportes_novembro = pd.read_csv('data/df_11.csv')
 transportes_dezembro = pd.read_csv('data/df_12.csv')
 
 
-tranporte_segundo_semestre= pd.concat([transportes_julho, transportes_agosto, transportes_setembro, transportes_outubro, transportes_novembro, transportes_dezembro], ignore_index=True)
+tranporte_ano= pd.concat([transportes_julho, transportes_agosto, transportes_setembro, transportes_outubro, transportes_novembro, transportes_dezembro], ignore_index=True)
 
-
-df_tran = tranporte_segundo_semestre[['CPF_CNPJ_Rem', 'TpRem', 'CPF_CNPJ_Des', 'TpDes', 'Volume']]
+tranporte_ano = convert_id_to_str(tranporte_ano)
+df_tran = tranporte_ano[['CPF_CNPJ_Rem', 'TpRem', 'CPF_CNPJ_Des', 'TpDes', 'Volume']]
 df_tran = df_tran.groupby(['CPF_CNPJ_Rem', 'TpRem', 'CPF_CNPJ_Des', 'TpDes'])['Volume'].sum().reset_index()
 
 #  Convertendo todos os nós para str
@@ -53,21 +58,20 @@ emp_type = {}
 
 
 
-df_nodes = tranporte_segundo_semestre[['CPF_CNPJ_Rem', 'TpRem']].rename(columns={'CPF_CNPJ_Rem': 'CPF_CNPJ', 'TpRem': 'Tipo'})
-df_nodes = pd.concat([df_nodes, tranporte_segundo_semestre[['CPF_CNPJ_Des', 'TpDes']].rename(columns={'CPF_CNPJ_Des': 'CPF_CNPJ', 'TpDes': 'Tipo'})]).drop_duplicates('CPF_CNPJ')
+df_nodes = tranporte_ano[['CPF_CNPJ_Rem', 'TpRem']].rename(columns={'CPF_CNPJ_Rem': 'CPF_CNPJ', 'TpRem': 'Tipo'})
+df_nodes = pd.concat([df_nodes, tranporte_ano[['CPF_CNPJ_Des', 'TpDes']].rename(columns={'CPF_CNPJ_Des': 'CPF_CNPJ', 'TpDes': 'Tipo'})]).drop_duplicates('CPF_CNPJ')
+
 df_pto  = df_tran[(df_tran['TpRem'] == 'PTO_IBAMA') & (df_tran['TpDes'] == 'PTO_IBAMA')].rename(columns={'CPF_CNPJ_Rem': 'CPF_CNPJ'})
 df_pto = df_pto.groupby('CPF_CNPJ')['Volume'].sum().reset_index()
 
-
-
-origem = tranporte_segundo_semestre[["CPF_CNPJ_Rem", "LatOrigem", "LongOrigem", "TpRem"]].rename(
+origem = tranporte_ano[["CPF_CNPJ_Rem", "LatOrigem", "LongOrigem", "TpRem"]].rename(
     columns={
     'CPF_CNPJ_Rem': 'id_emp', 
     'LatOrigem': 'latitude', 
     'LongOrigem': 'longitude', 
     'TpRem': 'type'
 })
-destino = tranporte_segundo_semestre[["CPF_CNPJ_Des", "LatDestino", "LongDestino", "TpDes"]].rename(
+destino = tranporte_ano[["CPF_CNPJ_Des", "LatDestino", "LongDestino", "TpDes"]].rename(
     columns={
     'CPF_CNPJ_Des': 'id_emp', 
     'LatDestino': 'latitude', 
@@ -85,8 +89,8 @@ for i,node in nodes.iterrows():
 
 get_timberflow(G, emp_type)
 #  Parte wal sug
-df_nodes = tranporte_segundo_semestre[['CPF_CNPJ_Rem', 'TpRem']].rename(columns={'CPF_CNPJ_Rem': 'CPF_CNPJ', 'TpRem': 'Tipo'})
-df_nodes = pd.concat([df_nodes, tranporte_segundo_semestre[['CPF_CNPJ_Des', 'TpDes']].rename(columns={'CPF_CNPJ_Des': 'CPF_CNPJ', 'TpDes': 'Tipo'})]).drop_duplicates('CPF_CNPJ')
+df_nodes = tranporte_ano[['CPF_CNPJ_Rem', 'TpRem']].rename(columns={'CPF_CNPJ_Rem': 'CPF_CNPJ', 'TpRem': 'Tipo'})
+df_nodes = pd.concat([df_nodes, tranporte_ano[['CPF_CNPJ_Des', 'TpDes']].rename(columns={'CPF_CNPJ_Des': 'CPF_CNPJ', 'TpDes': 'Tipo'})]).drop_duplicates('CPF_CNPJ')
 
 
 """
@@ -130,3 +134,18 @@ for x in aux:
 print(len(components_aux))
 print(components_aux)
 """
+
+manejos = set(df_nodes[df_nodes['Tipo'] == 'MANEJO']['CPF_CNPJ'])
+finais = set(df_nodes[df_nodes['Tipo'] == 'FINAL']['CPF_CNPJ'])
+all_important_patios = set(df_pto['CPF_CNPJ'])
+aux_valid_patios = set()
+valid_patios = set()
+# Verifica 
+
+for each_manejo in manejos:
+    T = nx.bfs_tree(G, each_manejo)
+    if finais.intersection(set(T.nodes())) != set():
+        # Salva os pátios que são conectados a um manejo
+        aux_valid_patios.update(all_important_patios.intersection(set(T.nodes())))
+
+print(len(aux_valid_patios))
--- src/important_companies/modules.py
+++ src/important_companies/modules.py
@@ -5,20 +5,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 from networkx.classes.function import path_weight
 from tqdm import tqdm
-
+from data_cleaner import convert_id_to_str
 
 
 def get_concessions(list_nodes: list,emp_type: dict)-> int: 
-  """As concessões são todas emps marcadas como MANEJO,1
-  (fonte legal e extratores de madeira)
-
-  Args:
-      list_nodes (list): _description_
-      emp_type (dict): _description_
-
-  Returns:
-      int: _description_
-  """
+  # As concessões são todas emps marcadas como MANEJO,1
+  # (fonte legal e extratores de madeira)
 
   count = 0
   for node in list_nodes:
@@ -28,18 +20,9 @@ def get_concessions(list_nodes: list,emp_type: dict)-> int:
   return count
 
 def get_sink_nodes(graph: nx.graph, emp_type: dict)-> dict:
-  """If node is marked as FINAL, he is a sink
-  It is the final destination of the timber chain
+  # If node is marked as FINAL, he is a sink
+  # It is the final destination of the timber chain
 
-
-  Args:
-      graph (nx.graph): grafo
-      emp_type (dict): diciionario em que a chave é o id do nó
-      e o value é seu tipo (MANEJO, PATIO OU FINAL)
-
-  Returns:
-      dict: _description_
-  """
   nodes = {}
   for node in graph.nodes():
     if emp_type[node] == 'FINAL':
@@ -74,3 +57,4 @@ def get_timberflow(graph: nx.graph,emp_type: dict) -> None:
       total_out += path_weight(graph, [edge[0],edge[1]], weight='Volume')
 
   print(f'Inflow Vol(m3): {total_in} \nOut Vol(m3): {total_out} \nProportion: {total_out/total_in}')
+  return total_in, total_out
